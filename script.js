2aecd7b929d018d9e51c7b1e4290c19b36a67b9d
mhameen <mhameen001@gmail.com>
2020-11-14 12:26:24 +0530

    js assignment

---
+++ script.js
@@ -0,0 +1,226 @@
+console.log ("if else if operation");
+let percentage= 65;
+if (percentage >= 85){
+console.log("distinction");
+}
+else if (percentage >= 40){
+console.log("second class");
+}
+else if (percentage >=60){
+console.log("first class");
+}
+else{
+console.log("fail");  
+}
+
+console.log("switch operation")
+
+let x=87;
+switch(x){
+ case 35:
+ console.log("just pass");
+ break;
+ case 36:
+ console.log("just pass");
+ break;
+ case 37:
+ console.log("just pass");
+ break;
+ case 38:
+ console.log("just pass");
+ break;
+ case 39:
+ console.log("just pass");
+ break;
+ case 41:
+ console.log("second class");   
+ break;
+  case 42:
+ console.log("second class");   
+ break;
+  case 43:
+ console.log("second class");   
+ break; 
+ case 44:
+ console.log("second class");   
+ break;
+ case 45:
+ console.log("second class");   
+ break;
+ case 46:
+ console.log("second class");   
+ break;
+ case 47:
+ console.log("second class");   
+ break;
+ case 48:
+ console.log("second class");   
+ break; 
+ case 49:
+ console.log("second class");   
+ break;
+ case 47:
+ console.log("second class");   
+ break;
+ case 48:
+ console.log("second class");   
+ break;
+ case 49:
+ console.log("second class");   
+ break; 
+ case 50:
+ console.log("second class");   
+ break;
+ case 51:
+ console.log("second class");   
+ break;
+ case 52:
+ console.log("second class");   
+ break;
+ case 53:
+ console.log("second class");   
+ break;
+ case 54:
+ console.log("second class");   
+ break;
+ case 55:
+ console.log("second class");   
+ break;
+ case 56:
+ console.log("second class");   
+ break;
+ case 57:
+ console.log("second class");   
+ break; 
+ case 58:
+ console.log("second class");   
+ break;
+ case 59:
+ console.log("second class");   
+ break;
+ case 60:
+ console.log("first class");
+ break;
+ case 61:
+ console.log("first class");   
+ break;
+ case 62:
+ console.log("first class");   
+ break;
+ case 63:
+ console.log("first class");   
+ break;
+ case 64:
+ console.log("first class");   
+ break;
+ case 65:
+ console.log("first class");   
+ break;
+ case 66:
+ console.log("first class");   
+ break;
+ case 67:
+ console.log("first class");   
+ break;
+ case 68:
+ console.log("first class");   
+ break;
+ case 69:
+ console.log("first class");   
+ break;
+ case 70:
+ console.log("first class");   
+ break;
+ case 71:
+ console.log("first class");   
+ break;
+ case 72:
+ console.log("first class");   
+ break;
+ case 73:
+ console.log("first class");   
+ break;
+ case 74:
+ console.log("first class");   
+ break;
+ case 75:
+ console.log("first class");   
+ break;
+ case 76:
+ console.log("first class");   
+ break; 
+ case 77:
+ console.log("first class");   
+ break;
+ case 78:
+ console.log("first class");   
+ break;
+ case 79:
+ console.log("first class");   
+ break;
+ case 80:
+ console.log("first class");   
+ break;
+ case 81:
+ console.log("first class");   
+ break;
+ case 82:
+ console.log("first class");   
+ break;
+ case 83:
+ console.log("first class");   
+ break;
+ case 84:
+ console.log("first class");   
+ break;
+ case 85:
+ console.log("distinction");   
+ break;
+ case 86:
+ console.log("distinction");   
+ break;
+ case 87:
+ console.log("distinction");   
+ break;
+ case 88:
+ console.log("distinction");   
+ break;
+ case 89:
+ console.log("distinction");   
+ break;
+ case 90:
+ console.log("distinction");   
+ break;
+ case 91:
+ console.log("distinction");   
+ break;
+ case 92:
+ console.log("distinction");   
+ break;
+ case 93:
+ console.log("distinction");   
+ break;
+ case 94:
+ console.log("distinction");   
+ break;
+ case 95:
+ console.log("distinction");   
+ break;
+ case 96:
+ console.log("distinction");   
+ break;
+ case 97:
+ console.log("distinction");   
+ break; 
+ case 98:
+ console.log("distinction");   
+ break; 
+ case 99:
+ console.log("distinction");   
+ break;
+ case 100:
+ console.log("distinction");   
+ break;                            
+ default:    
+ console.log("fail");
+}
