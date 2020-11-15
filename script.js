6f6b031913e220955e33d3b9402f2f59c5e4d85a
mhameen <mhameen001@gmail.com>
2020-11-15 17:24:01 +0530

    updated project js

--- script.js
+++ script.js
@@ -11,228 +11,37 @@ console.log("first class");
 }
 else{
 console.log("fail");  
-}
+} 
 
 console.log("switch operation")
-
-let x=87;
-switch(x){
- case 35:
- console.log("just pass");
- break;
- case 36:
- console.log("just pass");
- break;
- case 37:
- console.log("just pass");
- break;
- case 38:
- console.log("just pass");
- break;
- case 39:
- console.log("just pass");
- break;
- case 41:
- console.log("second class");   
- break;
-  case 42:
- console.log("second class");   
- break;
-  case 43:
- console.log("second class");   
- break; 
- case 44:
- console.log("second class");   
- break;
- case 45:
- console.log("second class");   
- break;
- case 46:
- console.log("second class");   
- break;
- case 47:
- console.log("second class");   
- break;
- case 48:
- console.log("second class");   
- break; 
- case 49:
- console.log("second class");   
- break;
- case 47:
- console.log("second class");   
- break;
- case 48:
- console.log("second class");   
- break;
- case 49:
- console.log("second class");   
- break; 
- case 50:
- console.log("second class");   
- break;
- case 51:
- console.log("second class");   
- break;
- case 52:
- console.log("second class");   
- break;
- case 53:
- console.log("second class");   
- break;
- case 54:
- console.log("second class");   
- break;
- case 55:
- console.log("second class");   
- break;
- case 56:
- console.log("second class");   
- break;
- case 57:
- console.log("second class");   
- break; 
- case 58:
- console.log("second class");   
- break;
- case 59:
- console.log("second class");   
- break;
- case 60:
- console.log("first class");
- break;
- case 61:
- console.log("first class");   
- break;
- case 62:
- console.log("first class");   
- break;
- case 63:
- console.log("first class");   
- break;
- case 64:
- console.log("first class");   
- break;
- case 65:
- console.log("first class");   
- break;
- case 66:
- console.log("first class");   
- break;
- case 67:
- console.log("first class");   
- break;
- case 68:
- console.log("first class");   
- break;
- case 69:
- console.log("first class");   
- break;
- case 70:
- console.log("first class");   
- break;
- case 71:
- console.log("first class");   
- break;
- case 72:
- console.log("first class");   
- break;
- case 73:
- console.log("first class");   
- break;
- case 74:
- console.log("first class");   
- break;
- case 75:
- console.log("first class");   
- break;
- case 76:
- console.log("first class");   
- break; 
- case 77:
- console.log("first class");   
- break;
- case 78:
- console.log("first class");   
- break;
- case 79:
- console.log("first class");   
- break;
- case 80:
- console.log("first class");   
- break;
- case 81:
- console.log("first class");   
- break;
- case 82:
- console.log("first class");   
- break;
- case 83:
- console.log("first class");   
- break;
- case 84:
- console.log("first class");   
- break;
- case 85:
- console.log("distinction");   
- break;
- case 86:
- console.log("distinction");   
- break;
- case 87:
- console.log("distinction");   
- break;
- case 88:
- console.log("distinction");   
- break;
- case 89:
- console.log("distinction");   
- break;
- case 90:
- console.log("distinction");   
- break;
- case 91:
- console.log("distinction");   
- break;
- case 92:
- console.log("distinction");   
- break;
- case 93:
- console.log("distinction");   
- break;
- case 94:
- console.log("distinction");   
- break;
- case 95:
- console.log("distinction");   
- break;
- case 96:
- console.log("distinction");   
- break;
- case 97:
- console.log("distinction");   
- break; 
- case 98:
- console.log("distinction");   
- break; 
- case 99:
- console.log("distinction");   
- break;
- case 100:
- console.log("distinction");   
- break;                            
- default:    
- console.log("fail");
+    let  marks=10;
+switch (true){
+    case marks>85:
+    console.log("distinction");
+    break;
+    case marks>40:
+    console.log("second class");
+    break;
+    case marks>60:
+    console.log("first class");
+    break;
+    case marks>35:
+    console.log("just passed");
+    break;
+    default:
+    console.log("fail");       
 }
 
 function studentresult(name,percentage){
-    console.log("my name is " + name + " and my percentage is " + percentage );
+    console.log("My name is " + name + " and my percentage is " + percentage );
 }
-studentresult("Disha Patni" , 70)
-studentresult("Sharuk Khan" , 30);
-studentresult("Salman Khan" , 34);
-studentresult("Rashmika Mandana" , 66);
-studentresult("Shraddha Kapoor" , 60);
+studentresult("Disha Patni",70)
+studentresult("Sharuk Khan",30);
+studentresult("Salman Khan",34);
+studentresult("Rashmika Mandana",66);
+studentresult("Shraddha Kapoor",60);
+
+
 
 
 
@@ -242,3 +51,6 @@ function input(temperature){
 }
 input(30)
 input(60)
+input(100)
+input(10)
+input(40)
